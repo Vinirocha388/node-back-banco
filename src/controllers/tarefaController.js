ab9c5d6768b02edf0b83814b363e8389c071880b
Vinícius Rocha <vinicius.a.rocha8@aluno.senai.br>
2025-03-11 16:13:11 -0300

    feat: add create method

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -9,13 +9,21 @@ class TarefaController {
     res.status(500).json({ erro: "Não foi possível buscar as tarefas" });
     }
   };
-  create = ({ body: { descricao } }, res) => {
-    if (!descricao) {
-      return res.status(400).json({ erro: "Descrição é obrigatória" });
+
+  create = (req, res) => {
+    const { descricao } = req.body;
+    try {
+      if (!descricao) {
+        return res.status(400).json({ erro: "Descrição é obrigatória" });
+      }
+      const novaTarefa = tarefaModel.create(descricao);
+      res.status(201).json(novaTarefa);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ erro: "Erro ao criar a tarefa" });
     }
-    const novaTarefa = tarefaModel.create(descricao);
-    res.status(201).json(novaTarefa);
   };
+
   update = ({ params: { id }, body: { concluida } }, res) => {
     const tarefaAtualizada = tarefaModel.update(id, concluida);
     if (!tarefaAtualizada) {
